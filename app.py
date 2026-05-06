ce8e5c3bda9730441fb0c8aaacab60c9be5d002d
mfelskismith <m.felskismith@gmail.com>
2026-05-05 21:35:12 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -31,13 +31,25 @@ col1, col2, col3 = st.columns([1, 3, 1])
 with col2:
     st.markdown(
         """
-        <div style="
-            text-align:center;
-            font-size:44px;
+        <style>
+        .main-title {
+            text-align: center;
+            font-size: 44px;
             font-family: 'Rubik Glitch', cursive;
-            line-height:1.1;
-            margin-bottom:30px;
-        ">
+            line-height: 1.1;
+            margin-bottom: 30px;
+        }
+
+        @media (max-width: 768px) {
+            .main-title {
+                font-size: 30px;
+                line-height: 1.15;
+                margin-bottom: 20px;
+            }
+        }
+        </style>
+
+        <div class="main-title">
             💀 Random Horror<br>
             Movie Picker 📼
         </div>
