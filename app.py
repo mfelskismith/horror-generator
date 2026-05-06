2c9d2583cd85dd293b51bff37ac39d089e192a1e
mfelskismith <m.felskismith@gmail.com>
2026-05-05 19:17:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import re
 st.set_page_config(page_title="Horror Generator", layout="centered")
 
 # ----------------------------
-# HORROR FONT IMPORT
+# HORROR FONT IMPORT (NOSIFER)
 # ----------------------------
 st.markdown("""
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Creepster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nosifer&display=swap');
 </style>
 """, unsafe_allow_html=True)
 
@@ -33,8 +33,8 @@ with col2:
         """
         <div style="
             text-align:center;
-            font-size:52px;
-            font-family: 'Creepster', cursive;
+            font-size:42px;
+            font-family: 'Nosifer', cursive;
             line-height:1.1;
             margin-bottom:30px;
         ">
@@ -185,7 +185,7 @@ if query:
 st.write(f"🎥 {len(filtered)} movies match your filters")
 
 # ----------------------------
-# BUTTON STYLE (CREEPER FONT)
+# BUTTON STYLE (NOSIFER FONT)
 # ----------------------------
 st.markdown("""
 <style>
@@ -202,8 +202,8 @@ div.stButton > button {
 /* button text */
 div.stButton > button p,
 div.stButton > button span {
-    font-size: 32px !important;
-    font-family: 'Creepster', cursive !important;
+    font-size: 24px !important;
+    font-family: 'Nosifer', cursive !important;
     letter-spacing: 1px;
 }
 
@@ -216,7 +216,7 @@ div.stButton > button:hover {
 """, unsafe_allow_html=True)
 
 clicked = st.button(
-    "🎲 Pick Random Horror Movie",
+    "🎲 Pick Random Horror Movie 🎲",
     use_container_width=True,
     type="primary"
 )
